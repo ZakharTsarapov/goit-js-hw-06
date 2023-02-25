861f8235a9c7a333c212c4b75611776a56cb4c62
ZakharTsarapov <tsarapovs@gmail.com>
2023-02-25 23:15:17 +0200

    Update task-10.js
    finish

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -5,5 +5,32 @@ function getRandomHexColor() {
 const refs = {
   onCreateBtn: document.querySelector("[data-create]"),
   onDestroyBtn: document.querySelector("[data-destroy]"),
-  
+  input: document.querySelector('[type="number"]'),
+  boxes: document.querySelector('#boxes'),
 };
+
+function createBoxes(amount) {
+  let size = 30;
+  let boxArray = [];
+
+
+  for (let i = 1; i <= amount; i += 1) {
+    let color = getRandomHexColor();
+    boxArray.push(`<div id="boxes-element" style="width:${size}px; height:${size}px; background-color:${color};"></div>`,
+    );
+    size += 10;
+  } return boxArray.join('');
+};
+
+refs.onCreateBtn.addEventListener("click", onCreateBoxesBtn);
+refs.onDestroyBtn.addEventListener("click", onDestroyBoxesBtn);
+
+function onCreateBoxesBtn() {
+  refs.boxes.insertAdjacentHTML("afterbegin", createBoxes(refs.input.value));
+  refs.boxes.style.cssText = "display:flex; gap:20px; flex-wrap:wrap;";
+}
+
+function onDestroyBoxesBtn() {
+  refs.input.value = "";
+  refs.boxes.innerHTML = "";
+}
